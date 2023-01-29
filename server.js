e012b016af1908bea5937b255855c9d5b028da64
CharlyneGalls <charlyne.rivera@gmail.com>
2023-01-29 18:20:06 +0100

    [init] installing dotenv

--- server.js
+++ server.js
@@ -1,18 +1,28 @@
 const tmi = require('tmi.js');
+require('dotenv').config();
 
 const client = new tmi.Client({
   connection: {
     reconnect: true,
   },
   channels: ['mlle_brightside'],
+  identity: {
+    username: `[Bot Username]`,
+    password: `[Bot oAuth Token]`,
+  },
 });
 
 client.connect();
 
 client.on('message', async (channel, context, message) => {
-  console.log('channel', {
-    channel,
-    user: context.username,
-    message,
-  });
+  const isNotBot =
+    context.username.toLowerCase() !==
+    process.env.TWITCH_BOT_USERNAME.toLowerCase();
+
+  if (isNotBot) {
+    client.say(
+      channel,
+      `Responding to ${context.username} message: ${message}`
+    );
+  }
 });
